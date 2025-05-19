0c5eaf9c0bd76ae9136882366a53853aa4ff7501
Akash Chaudhari <akashchaudhari1012@gmail.com>
2025-05-19 13:50:41 +0530

    static added:

--- Backend_Telegram/settings.py
+++ Backend_Telegram/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
